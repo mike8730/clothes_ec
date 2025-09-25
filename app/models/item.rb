a01ca2c61257f5cd41a345e23897108e12d36461
mike8730 <razerrush5v@gmail.com>
2025-09-25 22:19:17 +0900

    itemモデルの作成・マイグレーションファイルの実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
-  
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 0 }
+  validates :gender_id, presence: true, numericality: { other_than: 0 }
+
+  has_many :item_variants
+  belongs_to_active_hash :category
+  belongs_to_active_hash :gender
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_18_161725) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_25_120117) do
+  create_table "item_variants", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "gender_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_18_161725) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
