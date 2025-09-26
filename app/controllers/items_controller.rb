cdc2b99a43fa5306c901697a1504b5a314948cf5
mike8730 <razerrush5v@gmail.com>
2025-09-26 12:01:09 +0900

    商品出品ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,37 @@
 class ItemsController < ApplicationController
+
   def index
     @user = User.new
   end
+
+  def new
+     @item = Item.new
+     # フォームの初期状態でバリエーション入力欄を1つ表示するために build します。
+     @item.item_variants.build
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品を出品しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :price,
+      :description,
+      :category_id,
+      :gender_id,
+      # item_variants_attributes でネストした属性の受け取りを許可します。
+      # :id と :_destroy は更新・削除処理に必要です。
+      item_variants_attributes: [:id, :size_id, :color_id, :stock_quantity, :_destroy]
+    )
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_25_120117) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_26_023514) do
   create_table "colors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_25_120117) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
